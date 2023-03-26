06a95ba11f5690ae46ebcb2bbdcb9c722a93272c
ecospheree <121998521+ecospheree@users.noreply.github.com>
2023-03-26 23:32:49 +0700

    menambahkan class SayaTubeVideo

--- TPmodul6_1302210049/TPmodul6_1302210049/Program.cs
+++ TPmodul6_1302210049/TPmodul6_1302210049/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Diagnostics;
+
+public class SayaTubeVideo
+{
+    int id;
+    string title;
+    int playCount;
+
+    public SayaTubeVideo(String t)
+    {
+        this.title = t;
+        Random x = new Random();
+        id = x.Next(10000,99999);
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int pc)
+    {
+        this.playCount += pc;
+            
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("id : " + this.id);
+        Console.WriteLine("title : " + this.title);
+        Console.WriteLine("playCount : " + this.playCount);
+    }
+
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Nabiel");
+        video.IncreasePlayCount(11);
+        video.PrintVideoDetails();
+    }
+}
